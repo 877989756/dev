90b48eccc3319d252ae8e4d9e708af20eda42f8a
mobs <adfaliisjf@sjfo.com>
2021-09-09 00:47:57 +0800

    更新

---
+++ precache-manifest.60479ef5bef6e10c00fe0f5b3898437b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f326f13658c9a4cc907c",
+    "url": "/css/chunk-vendors.14bd940c.css"
+  },
+  {
+    "revision": "957c6018ae563519b2a0f426de20da36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27bae996cfb22b934365",
+    "url": "/js/app.89a50c72.js"
+  },
+  {
+    "revision": "02380f24a9286e96ac24",
+    "url": "/js/chunk-2d229481.cf8842c8.js"
+  },
+  {
+    "revision": "f326f13658c9a4cc907c",
+    "url": "/js/chunk-vendors.4234facc.js"
+  },
+  {
+    "revision": "211204d1e441b850750a8af0d7903c00",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
